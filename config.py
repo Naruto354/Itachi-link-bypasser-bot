7d5045a46b25112f5e6a40b854b05a24a8a3889c
Naruto354 <114726863+Naruto354@users.noreply.github.com>
2023-01-06 23:56:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
+import os
 
+
+class Config(object):
+    bot_token = os.environ.get("TOKEN", "")
+    api_hash = os.environ.get("HASH", "") 
+    api_id = os.environ.get("ID", "")
+    GDTot_Crypt = os.environ.get("CRYPT","b0lDek5LSCt6ZjVRR2EwZnY4T1EvVndqeDRtbCtTWmMwcGNuKy8wYWpDaz0%3D")
+    Laravel_Session = os.environ.get("Laravel_Session","")
+    XSRF_TOKEN = os.environ.get("XSRF_TOKEN","")
+    DCRYPT = os.environ.get("DRIVEFIRE_CRYPT","")
+    KCRYPT = os.environ.get("KOLOP_CRYPT","aWFicnVaNWh4TThRbzFqdkE2U2FKNmJOTWhvWkZmbWswaUFadTB5NXJ3RT0%3D")
+    HCRYPT = os.environ.get("HUBDRIVE_CRYPT","Q29hdlpLUEZTSEJLUjVZRkZQSExLODFuWGVudUlNK0ZPZlZmS1hENWxZVT0%3D")
+    KATCRYPT = os.environ.get("KATDRIVE_CRYPT","")
